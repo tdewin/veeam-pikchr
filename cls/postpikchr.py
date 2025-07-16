b8cb4e764166ca6d99aafa6b775cea250c10f47c
tdewin <tdewin@users.noreply.github.com>
2025-07-16 10:04:49 +0200

    matching on purely color now
    so that diagrams look okish without postprocessing
    
    also resizing svg diagrams that are not 100x100

--- cls/postpikchr.py
+++ cls/postpikchr.py
@@ -9,7 +9,7 @@ import platform
 
 from pathlib import Path
 
-def parse_svg_or_xml(tree,verbose,font,searchdir,scale=1,fontscale=1):
+def parse_svg_or_xml(tree,magic,verbose,font,searchdir,scale=1,fontscale=1):
     try:
 
         root = tree.getroot()
@@ -17,20 +17,21 @@ def parse_svg_or_xml(tree,verbose,font,searchdir,scale=1,fontscale=1):
         # Define the SVG namespace
         ns = {'svg': 'http://www.w3.org/2000/svg'}
 
+
+
         #for autoremove
         #$remove = 0xee00a0
         #stroke: rgb(238,0,160);
         path_elements = root.findall('.//svg:path', namespaces=ns)
         for e in path_elements:
           if "style" in e.attrib:
-            if "rgb(238,0,160)" in e.attrib["style"]:
+            if magic in e.attrib["style"]:
                 root.remove(e)
 
 
-	      # Find all <text> elements using XPath
+	# Find all <text> elements using XPath
         text_elements = root.findall('.//svg:text', namespaces=ns)
 
-        m = re.compile(r'^cls-([a-zA-Z0-9 -]+)')
         mpct = re.compile(r'([0-9\.]+)[%]')
         if font:
             newstyle = ET.Element('{http://www.w3.org/2000/svg}style',attrib={}) 
@@ -43,19 +44,24 @@ def parse_svg_or_xml(tree,verbose,font,searchdir,scale=1,fontscale=1):
         fblank=searchdir / "{}.svg".format("blank")
 
         for elem in text_elements:
+          escale = scale
           ctxt = ""
           if elem.text:
             ctxt = elem.text
-          clsm = m.match(ctxt)
-          if clsm:
-            #print(f"Text content: '{elem.text}' | Attributes: {elem.attrib}")
+          if ctxt != ""  and "fill" in elem.attrib and elem.attrib["fill"] == magic:
+            if verbose:
+              print(f"Text content: '{elem.text}' | Attributes: {elem.attrib}",file=sys.stderr)
             x=float(elem.attrib['x'])
             y=float(elem.attrib['y'])
-            idn=clsm.group(1)
+            idn=ctxt.lower().replace(" ","-")
+
+            sq = 100
+            wid = sq
+            ht = sq
 
           
-            newg = ET.Element('{http://www.w3.org/2000/svg}g',attrib={'transform': "translate({},{}) scale({})".format(x-(50*scale),y-(50*scale),scale),'id':idn}) 
-            newe = ET.Element('{http://www.w3.org/2000/svg}rect', attrib={'x': '0', 'y': '0', 'width': "{}".format(scale*100), 'height':"{}".format(scale*100) , 'fill':'green'})
+
+            newe = ET.Element('{http://www.w3.org/2000/svg}rect', attrib={'x': '0', 'y': '0', 'width': "{}".format(escale*wid), 'height':"{}".format(escale*ht) , 'fill':'#00ff00'})
 
             ftest=searchdir / "{}.svg".format(idn)
             if ftest.exists():
@@ -63,10 +69,18 @@ def parse_svg_or_xml(tree,verbose,font,searchdir,scale=1,fontscale=1):
                clsroot = clstree.getroot()
                gs=clsroot.findall('.//svg:g',namespaces=ns)
                if len(gs) > 0:
-                 newg.append(gs[0])
+                 if "viewBox" in clsroot.attrib:
+                   vb = clsroot.attrib["viewBox"]
+                   flarr = [float(x) for x in vb.split(' ')]
+                   if len(flarr) > 3:
+                     wid = flarr[2]
+                     ht = flarr[3]
+                     escale=sq/max(wid,ht)*escale
+                     if verbose:
+                       print(wid,ht,escale,file=sys.stderr)
+                 newe = gs[0]
                else:
                  print("ooops, no g's",file=sys.stderr)
-                 newg.append(newe) 
             elif fblank.exists():
                blktree = ET.parse(fblank)
                blkroot = blktree.getroot()
@@ -75,15 +89,15 @@ def parse_svg_or_xml(tree,verbose,font,searchdir,scale=1,fontscale=1):
                  txt=blkroot.findall('.//svg:text',namespaces=ns)
                  if len(txt) > 0:
                    txt[0].text = idn
-                 newg.append(gs[0])
-
+                 newe = gs[0]
                else:
                  print("ooops, no g's",file=sys.stderr)
-                 newg.append(newe) 
             else:
-              newg.append(newe) 
               print("ooops, no blank",file=sys.stderr)
+
             root.remove(elem)
+            newg = ET.Element('{http://www.w3.org/2000/svg}g',attrib={'transform': "translate({},{}) scale({})".format(x-(wid/2*escale),y-(ht/2*escale),escale),'id':idn}) 
+            newg.append(newe) 
             root.append(newg)
           else:
             if "font-size" in elem.attrib:
@@ -103,8 +117,9 @@ def main():
     parser = argparse.ArgumentParser(description="Parse SVG/XML from stdin.")
     parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
     parser.add_argument('--output', '-o', type=str, help='Output file path (default: stdout)')
-    parser.add_argument('--font', '-f', type=str, help='Font for update') 
-    parser.add_argument('--fontscale', '-r', type=float, help='Scale font') 
+    parser.add_argument('--magic', type=str, help='Magic code color for replacement (default: rgb(238,0,160))',default="rgb(238,0,160)")
+    parser.add_argument('--font', '-f', type=str, help='Font for update', default="monospace") 
+    parser.add_argument('--fontscale', '-r', type=float, help='Scale font', default=1.0) 
     parser.add_argument('--dir', '-d', type=Path, help='Search for svgs in this path',default=Path("."))
     parser.add_argument('--scale', '-s', type=float, help='Scale images',default=1.0)
     parser.add_argument('--clipboard','-c',action='store_true',help='Read from clipboard')
@@ -128,7 +143,7 @@ def main():
     ET.register_namespace('', 'http://www.w3.org/2000/svg')
 
 
-    tree = parse_svg_or_xml(tree,args.verbose,args.font,args.dir,args.scale,args.fontscale)
+    tree = parse_svg_or_xml(tree,args.magic,args.verbose,args.font,args.dir,args.scale,args.fontscale)
 
 
     if args.output:
