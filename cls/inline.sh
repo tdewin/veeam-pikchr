8d75460cb0be77b4e5dadcd91df0e3be4532a0fb
tdewin <tdewin@users.noreply.github.com>
2025-07-17 11:50:34 +0200

    Add files via upload

--- cls/inline.sh
+++ cls/inline.sh
@@ -1,1 +1,5 @@
-./pikchr --svg-only /data/$NAME.pic | ./postpikchr.py -d /icons -o /data/$NAME.svg
+echo "Building from /data/$NAME.pic"
+./pikchr --svg-only "/data/$NAME.pic" | ./postpikchr.py -f "$FONT" -d /icons -o "/data/$NAME.svg"
+
+echo "Converting to png /data/$NAME.png /data/$NAME.svg"
+rsvg-convert  --stylesheet "/fonts/font.css"  -o "/data/$NAME.png" "/data/$NAME.svg"
--- cls/sample.sh
+++ cls/sample.sh
@@ -1,3 +1,5 @@
 NAME=/data/veeamdiagram
+FONT="ubuntu mono"
 ./prepikchr.py --title Test vbr proxy repo -c proxy,repo --filename $NAME.pic
-./pikchr --svg-only $NAME.pic | ./postpikchr.py -d /icons -o $NAME.svg
+./pikchr --svg-only $NAME.pic | ./postpikchr.py -f "$FONT" -d /icons -o $NAME.svg
+rsvg-convert  -o $NAME.png $NAME.svg
